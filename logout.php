1387704d148d17da72bbbfed000c9a9c259362f7
shadialahmd <shadi.alahmd@gmail.com>
2023-08-03 13:23:09 +0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,13 @@
 <?php
   require "php/db_connection.php";
 
-  if($con) {
-    $query = "UPDATE admin_credentials SET IS_LOGGED_IN = 'false'";
-    $result = mysqli_query($con, $query);
-  }
+  // if($con) {
+  //   $query = "UPDATE admin_credentials SET IS_LOGGED_IN = 'false'";
+  //   $result = mysqli_query($con, $query);
+
+  // }
+  header("Location: login.php");
+exit();
 ?>
 
 <!DOCTYPE html>
